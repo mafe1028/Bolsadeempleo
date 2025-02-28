5e5f6565c20ed07e4cf72f28bc741588b1ef5001
mafe ceballos <mmarino@ucentral.edu.co>
2025-02-27 21:13:10 -0500

    (nuevo primer commit de estructura)

---
+++ Bolsa-de-empleo/src/main/java/uc/central/Bolsa/de/empleo/BolsaDeEmpleoApplication.java
@@ -0,0 +1,13 @@
+package uc.central.Bolsa.de.empleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaDeEmpleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsaDeEmpleoApplication.class, args);
+	}
+
+}
---
+++ Bolsa-de-empleo/src/test/java/uc/central/Bolsa/de/empleo/BolsaDeEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package uc.central.Bolsa.de.empleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaDeEmpleoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
