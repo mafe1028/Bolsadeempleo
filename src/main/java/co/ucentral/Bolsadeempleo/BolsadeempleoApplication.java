25feb3fe57c13e63612717ab97552f068f3346ec
mafe ceballos <mmarino@ucentral.edu.co>
2025-03-08 12:12:34 -0500

    nuevo

---
+++ src/main/java/co/ucentral/Bolsadeempleo/BolsadeempleoApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.Bolsadeempleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsadeempleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsadeempleoApplication.class, args);
+	}
+
+}
